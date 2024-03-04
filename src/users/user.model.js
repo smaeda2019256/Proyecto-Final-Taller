b5197a0cb0af0cccbb829ddab764b18057a6999f
smaeda2019256 <smaeda-2019256@Kinal.edu.gt>
2024-03-04 10:55:52 -0600

    Se crea modelo de Usuario

---
+++ src/users/user.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'The name is required']
+    },
+    email: {
+        type: String,
+        required: [true, 'The email is required'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'The password is required']
+    },
+    rol: {
+        type: String, 
+        required: true, 
+        default: 'CLIENTE_ROL'
+    },
+    state: {
+        type: Boolean,
+        default: true
+    },
+    cart: {
+        type: Array,
+        default: []
+    }
+});
+
+export default mongoose.model('User', UserSchema);
