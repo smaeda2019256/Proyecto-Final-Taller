64aa44fd7e35b605e879efab4725c76769655fc3
smaeda2019256 <smaeda-2019256@Kinal.edu.gt>
2024-03-05 19:07:47 -0600

    Se crea el validacion para ver si el role es valido

--- src/helpers/db-validators.js
+++ src/helpers/db-validators.js
@@ -1,1 +1,9 @@
 import User from '../users/user.model.js';
+import Role from '../users/role.js';
+
+export const isRoleValid = async (rol = '') => {
+    const existeRol = await Role.findOne({rol});
+    if(!existeRol) {
+        throw new Error(`The role ${ role } is not registered in the DB`);
+    }
+}
