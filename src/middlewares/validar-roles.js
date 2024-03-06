125fc75a4f7ca036a341fcfc7261df012593d613
smaeda2019256 <smaeda-2019256@Kinal.edu.gt>
2024-03-05 19:37:02 -0600

    Se crea verficacion si es ADMIN

--- src/middlewares/validar-roles.js
+++ src/middlewares/validar-roles.js
@@ -1,1 +1,20 @@
 import { request, response } from "express";
+
+
+export const isAdminRole = (req = request, res = response, next) => {
+    if(!req.user) {
+        return res.status(500).json({
+            msg: 'It is required to verify the ROLE without validating the TOKEN first'
+        });
+    }
+
+    const {rol, name} = req.user;
+
+    if(rol !== 'ADMIN_ROLE'){
+        return res.status(500).json({
+            msg: `${name} is not ADMIN - Does not have ACCESS to this function`
+        });
+    }
+
+    next();
+}
