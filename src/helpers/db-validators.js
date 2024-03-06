d0cfed00a8105734bdf929e71fd223ca34478347
smaeda2019256 <smaeda-2019256@Kinal.edu.gt>
2024-03-05 19:14:14 -0600

    Se crea la validacion para ver si existe el email y el id del usuario

--- src/helpers/db-validators.js
+++ src/helpers/db-validators.js
@@ -4,6 +4,20 @@ import Role from '../users/role.js';
 export const isRoleValid = async (rol = '') => {
     const existeRol = await Role.findOne({rol});
     if(!existeRol) {
-        throw new Error(`The role ${ role } is not registered in the DB`);
+        throw new Error(`The role: ${ role } is not registered in the DB`);
+    }
+}
+
+export const existsEmail = async (email = '') => {
+    const existeEmail = await User.findOne({email});
+    if(existeEmail) {
+        throw new Error(`The email: ${email} already exists and is registered in the DB`);
+    }
+}
+
+export const existsUserById = async (id) => {
+    const existeUser = await User.findById(id);
+    if(!existeUser) {
+        throw new Error(`The id: ${ id } does not exist in DB`);
     }
 }
