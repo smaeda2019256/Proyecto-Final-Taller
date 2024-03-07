943cf740ebe7807e42fc452ba50fcdd832aca8fe
smaeda2019256 <smaeda-2019256@Kinal.edu.gt>
2024-03-06 19:55:17 -0600

    Se crea la ruta del usuario en el server

--- configs/server.js
+++ configs/server.js
@@ -5,11 +5,13 @@ import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan'; 
 import { dbConnection } from "./mongo.js";
+import userRoutes from '../src/users/user.routes.js';
 
 class Server {
     constructor(){
         this.app = express();
         this.port = process.env.PORT;
+        this.userPath = '/pf/v2/users';
 
         this.conectarDB();
         this.middlewares();
@@ -29,7 +31,7 @@ class Server {
     }
 
     routes(){
-        
+        this.app.use(this.userPath, userRoutes);
     }
 
     listen(){
