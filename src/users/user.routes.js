6166207be5d221a44558b9621a137441b876fe67
smaeda2019256 <smaeda-2019256@Kinal.edu.gt>
2024-03-06 18:16:36 -0600

    Se crea la ruta del get y agregar del usuario

--- src/users/user.routes.js
+++ src/users/user.routes.js
@@ -1,3 +1,23 @@
 import { Router } from "express";
 import { check } from "express-validator";
-import { getUsers, postUser, putUser, deleteUser } from "./user.controlador";
+import { getUsers, postUser, putUser, deleteUser } from "./user.controlador.js";
+import { isRoleValid, existsEmail, existsUserById } from "../helpers/db-validators.js";
+import { validarCampos } from "../middlewares/validar-campos.js";
+import { validateJWT } from "../middlewares/validar-jwt.js";
+import { hasRole } from "../middlewares/validar-roles.js";
+
+const router = Router();
+
+router.get('/', getUsers);
+
+router.post('/',
+    [
+        check('name', 'The name ir required').not().isEmpty(),
+        check('password', 'The password must be more than 6 digits long').isLength( { min: 6 } ),
+        check('email', 'The email is not valid').isEmail(),
+        check('email').custom( existsEmail ),
+        validarCampos,
+    ], postUser
+);
+
+export default router;
