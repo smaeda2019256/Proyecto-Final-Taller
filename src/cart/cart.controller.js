b4bf0a49c0aaf6c8e66990a19479379937e4f55e
smaeda2019256 <smaeda-2019256@Kinal.edu.gt>
2024-03-08 11:30:02 -0600

    Se crea el post para ek Carrito

--- src/cart/cart.controller.js
+++ src/cart/cart.controller.js
@@ -3,6 +3,43 @@ import User from '../users/user.model.js';
 import user from '../users/user.model.js';
 import Product from '../products/producto.model.js';
 
+export const postCart = async(req, res) => {
+    const {itemId} = req.params;
+
+    try{
+        const item = await Product.findById(itemId);
+
+        if(!item){
+            return res.status(404).json({
+                message: "Product NOT IN EXISTENCE"
+            });
+        }
+
+        const idUser = req.user.id;
+        const {cart} = await User.findById(idUser);
+
+        const existsInCart = cart.find((item) => item.itemId === itemId);
+        
+        if (existsInCart){
+            existsInCart.quantity += 1;
+            existsInCart.name = item.name;
+            existsInCart.precio = item.precio;
+        }else {
+            cart.push({itemId, quantity: 1});
+        }
+
+        await User.findByIdAndUpdate(idUser, {cart});
+        return res.status(500).json({
+            message: "Product is ADDED to Cart"
+        });
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({
+            message: "ERROR | Internal Server"
+        });
+    }
+}
+
 export const getCart = async(req, res) => {
     try{
         const idUser = req.user.id;
@@ -18,3 +55,4 @@ export const getCart = async(req, res) => {
         });
     }
 };
+
