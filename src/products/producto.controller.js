c6e44fbb16fb7e616b529e097efab41683a8964e
smaeda2019256 <smaeda-2019256@Kinal.edu.gt>
2024-03-07 20:41:26 -0600

    Se crea el post para producto

--- src/products/producto.controller.js
+++ src/products/producto.controller.js
@@ -12,8 +12,30 @@ export const getProducts = async (req = request, res = response) => {
     ]);
     
     res.json({
-        msg: 'List of ACTIVE PRODUCTS',
+        msg: 'List - ACTIVE PRODUCTS',
         listaProducts
     });
 
+}
+
+export const postProducts = async (req = request, res = response) => {
+    const { estado, user, ...body } = req.body;
+    const productDB = await Producto.findOne({ name: body.name });
+
+    if (productDB) {
+        return res.status(400).json({
+            msg: `The Product ${ productDB.name }, already EXISTS in DataBase`
+        });
+    }
+
+    const data = {
+        ...body,
+        name: body.name.toUpperCase(),
+        user: req.user.id
+    }
+
+    const product = await Producto(data);
+    await product.save();
+
+    res.status(201).json(product);
 }
