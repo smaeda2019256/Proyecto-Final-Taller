ccd479a664ab891af856337e8ea5e8421f1bdfcf
smaeda2019256 <smaeda-2019256@Kinal.edu.gt>
2024-03-08 15:30:40 -0600

    Se crean la ruta del carrito en el Server

--- configs/server.js
+++ configs/server.js
@@ -10,6 +10,7 @@ import authRoutes from '../src/auth/auth.routes.js';
 import productRoutes from '../src/products/producto.routes.js';
 import categoryRoutes from '../src/category/categoria.routes.js';
 import searchRoutes from '../src/products/buscarPYC.routes.js';
+import cartRoutes from '../src/cart/cart.routes.js';
 
 class Server {
     constructor(){
@@ -20,6 +21,7 @@ class Server {
         this.productPath = '/pf/v2/products';
         this.categoryPath = '/pf/v2/categories';
         this.searchPath = '/pf/v2/search';
+        this.cartPath = '/pf/v2/cart';
 
         this.conectarDB();
         this.middlewares();
@@ -44,6 +46,7 @@ class Server {
         this.app.use(this.productPath, productRoutes);
         this.app.use(this.categoryPath, categoryRoutes);
         this.app.use(this.searchPath, searchRoutes);
+        this.app.use(this.cartPath, cartRoutes);
     }
 
     listen(){
