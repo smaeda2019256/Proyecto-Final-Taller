e057aec7cae428b01a14fc139b4c2f8e7ca17d20
smaeda2019256 <smaeda-2019256@Kinal.edu.gt>
2024-03-07 21:32:19 -0600

    Se termina el modelo de categoria

--- src/category/categoria.model.js
+++ src/category/categoria.model.js
@@ -1,7 +1,21 @@
 import mongoose from "mongoose";
 
 const CategorySchema = mongoose.Schema({
-    
+    name: {
+        type: String,
+        required: [true, 'The name is required'],
+        unique: true
+    },
+    estado: {
+        type: Boolean,
+        default: true,
+        required: true
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
 })
 
 export default mongoose.model('Category', CategorySchema);
